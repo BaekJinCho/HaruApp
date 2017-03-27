7380f76bbc3d06c0e63eb238901371b200d312af
조백진 <bjjo6457@gmail.com>
2017-03-27 20:40:42 +0900

    Add NavigationController at TutorialView and Moved Signup Login Page

--- Haru/Haru/HRTutorialViewController.m
+++ Haru/Haru/HRTutorialViewController.m
@@ -7,6 +7,8 @@
 //
 
 #import "HRTutorialViewController.h"
+#import "HRSignupViewController.h"
+//#import "HRLoginViewController.h"
 
 @interface HRTutorialViewController ()
 <UIScrollViewDelegate>
@@ -40,6 +42,16 @@
     
     
 }
+- (void)viewWillAppear:(BOOL)animated {
+    [super viewWillAppear:animated];
+    self.navigationController.navigationBarHidden = YES;
+}
+
+- (void)viewWillDisappear:(BOOL)animated {
+    [self.navigationController setNavigationBarHidden:NO animated:animated];
+    [super viewWillDisappear:animated];
+}
+
 - (IBAction)changePage:(UIPageControl *)sender {
     [self.tutorialScrollView setContentOffset:CGPointMake([sender currentPage] * self.view.frame.size.width, 0) animated:YES];
 }
@@ -49,6 +61,21 @@
     CGFloat position = [tutorialSrollView contentOffset].x / self.view.frame.size.width;
     self.tutorialPageControl.currentPage = position;
     
+}
+
+- (IBAction)clickSignupButton:(UIButton *)sender {
+    UIStoryboard *signupStoryboard = [UIStoryboard storyboardWithName:@"HRSignup" bundle:nil];
+    HRSignupViewController *signupView = [signupStoryboard instantiateViewControllerWithIdentifier:@"HRSignupViewController"];
+    [self.navigationController pushViewController:signupView animated:YES];
+    
+}
+
+- (IBAction)clickLoginButton:(UIButton *)sender {
+    /*
+    UIStoryboard *loginStoryboard = [UIStoryboard storyboardWithName:@"HRLogin" bundle:nil];
+    HRLoginViewController *loginView = [loginStoryboard instantiateViewControllerWithIdentifier:@"HRLoginViewController"];
+    [self.navigationController pushViewController:loginView animated:YES];
+     */
 }
 
 - (void)didReceiveMemoryWarning {
