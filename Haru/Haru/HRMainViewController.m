a3221f8fc279e3f19219a30efb5e454f4e306ce0
조백진 <bjjo6457@gmail.com>
2017-04-19 12:19:14 +0900

    Add Button In Mainview

--- Haru/Haru/HRMainViewController.m
+++ Haru/Haru/HRMainViewController.m
@@ -9,12 +9,29 @@
 #import "HRMainViewController.h"
 #import "HRCustomTableViewCell.h"
 #import "HRDetailViewController.h"
+#import "AddViewController.h"
 
 @interface HRMainViewController ()
-<UITableViewDelegate, UITableViewDataSource>
+<UITableViewDelegate, UITableViewDataSource,UINavigationControllerDelegate,UIImagePickerControllerDelegate>
 
 @property (weak, nonatomic) IBOutlet UITableView *mainTableView;
 @property (nonatomic) UIRefreshControl *refreshControl;
+
+//여러 버튼이 나오기 위한 Property
+@property (weak, nonatomic) IBOutlet UIButton *addButton;
+@property (weak, nonatomic) IBOutlet UIButton *libraryDirectButton;
+@property (weak, nonatomic) IBOutlet UIButton *cameraDirectButton;
+@property (weak, nonatomic) IBOutlet UIButton *writeButton;
+
+@property (weak, nonatomic) IBOutlet UIButton *backgroundButton;
+
+@property (weak, nonatomic) IBOutlet NSLayoutConstraint *addButtonTrailing;
+@property (weak, nonatomic) IBOutlet NSLayoutConstraint *addButtonBottom;
+@property (weak, nonatomic) IBOutlet NSLayoutConstraint *cameraButtonTrailing;
+@property (weak, nonatomic) IBOutlet NSLayoutConstraint *libraryButtonTrailing;
+@property (weak, nonatomic) IBOutlet NSLayoutConstraint *libraryButtonBottom;
+@property (weak, nonatomic) IBOutlet NSLayoutConstraint *writeButtonBottom;
+@property (strong, nonatomic) UIImage *pickedImage;
 
 @end
 
@@ -151,7 +168,7 @@
 - (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
 //    HRDetailViewController *detailViewContent = [HRDetailViewController new];
     
-    if([segue.identifier isEqualToString:@"DetailViewFromMainView"]) {
+    if ([segue.identifier isEqualToString:@"DetailViewFromMainView"]) {
         /***********************************Server 통신할 때, 필요*****************************************/
 //        HRPostModel *haruData = [[HRDataCenter sharedInstance] contentDataAtIndexPath:(NSIndexPath *)sender];
 //        
@@ -164,6 +181,13 @@
         detailViewController.indexPath               = (NSIndexPath *)sender;
         detailViewController.realmData = [realmDataInformation objectAtIndex:((NSIndexPath *)sender).row];
         
+    } else if ([segue.identifier isEqualToString:@"segueFromLibrary"]) {
+        UINavigationController *navi = (UINavigationController *)[segue destinationViewController];
+        AddViewController *addViewContent = (AddViewController *)[[navi viewControllers] objectAtIndex:0];
+        
+        UIImage *image = (UIImage *)sender;
+        
+        addViewContent.image = image;
     }
 }
 
@@ -187,6 +211,140 @@
     }
 
 }
+
+
+
+# pragma mark - Button Animation
+
+- (IBAction)clickedAddButton:(UIButton *)sender {
+    
+    [self buttonAnimationWhenClicked];
+}
+
+
+- (IBAction)clickedBackgroundButton:(UIButton *)sender {
+    
+    [self buttonAnimationWhenClicked];
+}
+
+- (void)buttonAnimationWhenClicked {
+    if (self.addButton.isSelected) {
+        [self.addButton setImage:[UIImage imageNamed:@"addButton"] forState:UIControlStateNormal];
+        [UIView animateWithDuration:0.3 animations:^{
+            
+            [self settingAlphaForButtonAnimation:0 writeBtnAlpha:0 libraryBtnAlpha:0 cameraBtnAlpha:0];
+            [self settingConstantForButtonAnimation:20 cameraBtnTrailing:20 libraryBtnBottom:self.addButtonBottom.constant libraryBtnTrailing:self.addButtonTrailing.constant];
+            
+            [self.addButton setSelected:NO];
+            [self.view layoutIfNeeded];
+        }];
+    } else {
+        [self.addButton setImage:[UIImage imageNamed:@"addButtonSelected"] forState:UIControlStateNormal];
+        [UIView animateWithDuration:0.3 animations:^{
+            
+            NSUInteger buttonSize = self.addButton.frame.size.width;
+            
+            [self settingAlphaForButtonAnimation:0.3 writeBtnAlpha:1 libraryBtnAlpha:1 cameraBtnAlpha:1];
+            [self settingConstantForButtonAnimation:buttonSize * 2.5 cameraBtnTrailing:buttonSize * 2.5 libraryBtnBottom:buttonSize * 1.8 libraryBtnTrailing:buttonSize * 1.8];
+            
+            [self.addButton setSelected:YES];
+            [self.view layoutIfNeeded];
+        }];
+    }
+};
+
+
+- (IBAction)clickedLibraryDirectButton:(id)sender {
+    if ([UIImagePickerController isSourceTypeAvailable:UIImagePickerControllerSourceTypePhotoLibrary == YES]) {
+        
+        UIImagePickerController *libraryPicker = [[UIImagePickerController alloc] init];
+        libraryPicker.sourceType = UIImagePickerControllerSourceTypePhotoLibrary;
+        libraryPicker.delegate = self;
+        libraryPicker.allowsEditing = YES;
+        
+        [self presentViewController:libraryPicker animated:YES completion:nil];
+        
+    } else {
+        UIAlertController *alertController = [UIAlertController alertControllerWithTitle:@"경고" message:@"사진 라이브러리에 접근할 수 없습니다.\n 설정을 다시 확인해주세요" preferredStyle:UIAlertControllerStyleAlert];
+        
+        UIAlertAction *alertAction = [UIAlertAction actionWithTitle:@"확인" style:UIAlertActionStyleCancel handler:^(UIAlertAction * _Nonnull action) {
+            [self dismissViewControllerAnimated:YES completion:nil];
+        }];
+        [alertController addAction:alertAction];
+    };
+};
+
+- (IBAction)clickedCameraDirectButton:(id)sender {
+    if ([UIImagePickerController isCameraDeviceAvailable:UIImagePickerControllerCameraDeviceFront]) {
+        
+        UIImagePickerController *cameraPicker = [[UIImagePickerController alloc] init];
+        cameraPicker.sourceType = UIImagePickerControllerSourceTypeCamera;
+        cameraPicker.delegate = self;
+        cameraPicker.allowsEditing = YES;
+        
+        [self presentViewController:cameraPicker animated:YES completion:nil];
+        
+    } else {
+        UIAlertController *alertController = [UIAlertController alertControllerWithTitle:@"경고" message:@"카메라에 접근할 수 없습니다.\n설정을 다시 확인해주세요" preferredStyle:UIAlertControllerStyleAlert];
+        
+        UIAlertAction *alertAction = [UIAlertAction actionWithTitle:@"확인" style:UIAlertActionStyleCancel handler:^(UIAlertAction * _Nonnull action) {
+            [self dismissViewControllerAnimated:YES completion:nil];
+        }];
+        [alertController addAction:alertAction];
+        [self presentViewController:alertController animated:YES completion:nil];
+    };
+}
+
+
+- (void)imagePickerController:(UIImagePickerController *)picker didFinishPickingMediaWithInfo:(NSDictionary<NSString *,id> *)info {
+    
+    UIImage *chosenImage = [info objectForKey:UIImagePickerControllerEditedImage];
+    self.pickedImage = chosenImage;
+    
+    if (picker.sourceType == UIImagePickerControllerSourceTypeCamera) {
+        dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
+            UIImageWriteToSavedPhotosAlbum(self.pickedImage, nil, nil, nil);
+        });
+    } else {
+        NSLog(@"라이브러리 진입");
+    }
+    
+    [self dismissViewControllerAnimated:YES completion:nil];
+    [self performSegueWithIdentifier:@"segueFromLibrary" sender:chosenImage];
+    
+}
+
+
+- (void)imagePickerControllerDidCancel:(UIImagePickerController *)picker {
+    
+    [self dismissViewControllerAnimated:YES completion:nil];
+}
+
+
+#pragma mark - SUPPORTING METHODS
+
+#pragma mark - Supporting button animation methods
+
+- (void)settingAlphaForButtonAnimation:(CGFloat)backgroundAlpha
+                          writeBtnAlpha:(CGFloat)writeAlpha
+                        libraryBtnAlpha:(CGFloat)libraryAlpha
+                         cameraBtnAlpha:(CGFloat)cameraAlpha {
+    
+    self.backgroundButton.alpha = backgroundAlpha;
+    self.writeButton.alpha = writeAlpha;
+    self.libraryDirectButton.alpha = libraryAlpha;
+    self.cameraDirectButton.alpha = cameraAlpha;
+}
+
+
+- (void)settingConstantForButtonAnimation:(CGFloat)writeBtnConstant cameraBtnTrailing:(CGFloat)cameraBtnConstant libraryBtnBottom:(CGFloat)libraryBtnBottomConstant libraryBtnTrailing:(CGFloat)libraryBtnTrailingConstant {
+    
+    self.writeButtonBottom.constant = writeBtnConstant;
+    self.cameraButtonTrailing.constant = cameraBtnConstant;
+    self.libraryButtonBottom.constant = libraryBtnBottomConstant;
+    self.libraryButtonTrailing.constant = libraryBtnTrailingConstant;
+}
+
 
 
 
