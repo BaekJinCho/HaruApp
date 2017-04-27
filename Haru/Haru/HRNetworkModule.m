2fe8d2810db6875b114e7dd0bd89fe5cc0f50e65
SangMin,Yeo <devssgg@gmail.com>
2017-04-27 19:50:02 +0900

    Add/Remove some Class

--- Haru/Haru/HRNetworkModule.m
+++ Haru/Haru/HRNetworkModule.m
@@ -7,9 +7,19 @@
 //
 
 #import "HRNetworkModule.h"
+#import <AFNetworking/AFNetworking.h>
+#import "HRConstantKeys.h"
+
+@interface HRNetworkModule ()
+
+@property (nonatomic) AFHTTPSessionManager *manager;
+@property (nonatomic) HRNetworkModule *networkModule;
+
+@end
 
 
 @implementation HRNetworkModule
+
 
 #pragma mark - Account Request
 
@@ -286,6 +296,60 @@
     
     completionHandler(YES, responseData);
 }
+
+//>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+//  Created by SSangGA on 2017. 3. 27..
+//  Copyright © 2017년 jcy. All rights reserved.
+//>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+
+//UserID요청하는 메소드
+- (void)getUserProfile:(CompletionBlock)completion
+{
+    NSURLSessionConfiguration *configuration = [NSURLSessionConfiguration defaultSessionConfiguration];
+    
+    self.manager = [[AFHTTPSessionManager manager] initWithSessionConfiguration:configuration];
+    
+    self.manager.requestSerializer = [AFJSONRequestSerializer serializer];
+    NSString *url = [NSString stringWithFormat:@"%@%@", BASIC_URL2, USER_URL];
+    
+    [self.manager.requestSerializer setValue:[@"Token " stringByAppendingString:[HRDataCenter sharedInstance].userToken] forHTTPHeaderField:TOKEN_KEY];
+    NSLog(@"url = %@",url);
+    [self.manager GET:url parameters:nil progress:nil success:^(NSURLSessionDataTask * _Nonnull task, id  _Nullable responseObject) {
+        completion(YES, responseObject);
+        NSLog(@"UserID RESPONSE:%@", responseObject);
+    } failure:^(NSURLSessionDataTask * _Nullable task, NSError * _Nonnull error) {
+        NSLog(@"UserID ERROR:%@", error);
+    }];
+}
+
+//postlist요청하는 메소드
+- (void)postListRequest:(NSString *)token completion
+                       :(CompletionBlock)completion
+{
+    NSURLSessionConfiguration *configuration = [NSURLSessionConfiguration defaultSessionConfiguration];
+    
+    self.manager = [[AFHTTPSessionManager manager] initWithSessionConfiguration:configuration];
+    //    manager.requestSerializer = [AFJSONRequestSerializer serializer];
+    
+    //    NSString *url = [NSString stringWithFormat:@"%@%@", BASIC_URL, POST_URL];
+    NSString *url = [NSString stringWithFormat:@"https://haru.ycsinabro.com/post/"];
+    
+    [self.manager.requestSerializer setValue:[@"Token " stringByAppendingString:[HRDataCenter sharedInstance].userToken] forHTTPHeaderField:TOKEN_KEY];
+    NSString *tokenValue = [NSString stringWithFormat:@"%@",[HRDataCenter sharedInstance].userToken];
+    NSLog(@"tokenValue = %@",tokenValue);
+    
+    [self.manager GET:url parameters:nil progress:nil success:^(NSURLSessionDataTask * _Nonnull task, id  _Nullable responseObject) {
+        NSLog(@"POSTLIST DATA:%@", responseObject);
+        completion(YES, responseObject);
+    } failure:^(NSURLSessionDataTask * _Nullable task, NSError * _Nonnull error) {
+        NSLog(@"POSTLIST ERROR:%@", error);
+        completion(NO,nil);
+    }];
+}
+
+//프로파일 이미지 요청 메소드
+//- (void)profileImageRequest:(NSString)
+
 
 
 
